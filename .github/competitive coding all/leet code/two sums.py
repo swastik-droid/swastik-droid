16d77963bf2f10677fb6f29bffcefa1f09fb64ad
swastik <80709615+swastik-droid@users.noreply.github.com>
2023-01-01 01:33:04 +0530

    finally

--- .github/competitive coding all/leet code/two sums.py
+++ .github/competitive coding all/leet code/two sums.py
@@ -1,17 +1,19 @@
 x=int(input())
 nums=[]
 for i in range(0,x):
-    f=input()
+    f=int(input())
     nums.append(f)
 print(nums)    
 y=int(input("target="))
-
-for j in nums:
-   a=nums[j]
-   for k in nums:
-       k =j+1
-       b=nums[k]
-       if (a+b)==y:
-          print(nums[j],nums[k])
-       else:
-          continue   
+a=int()
+b=int()
+for j,k in enumerate(nums):
+  a=k
+  c=j+1
+  for n in range(c,len(nums)):
+     b=nums[n]
+     z=a+b
+     if z==y:
+        print("[{0},{1}]".format(j,n))
+     else:
+        continue      
